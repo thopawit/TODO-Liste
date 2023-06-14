ae5f999f7adf54b1f80445930edf8a2973092f91
thopawit <thopuiwi@gmail.com>
2023-06-14 21:34:40 +0200

    Version 0.1 pre Jannik code variation

---
+++ src/DATA.java
@@ -0,0 +1,17 @@
+public interface DATA {
+    public String GetName();
+
+    public void SetName(String nameN);
+
+    public int GetDuration();
+
+    public void SetDuration(int durationN);
+
+    public boolean GetStatus();
+
+    public void SetStatus(boolean statusN);
+
+    public int GetPriority();
+
+    public void SetPriority(String priorityN);
+}
